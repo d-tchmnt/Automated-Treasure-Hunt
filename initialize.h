2045b73635f2d6b078b23173815c8b13075eb84e
d-tchmnt <53524672+d-tchmnt@users.noreply.github.com>
2019-07-31 18:31:26 +0300

    Create initialize.h

---
+++ initialize.h
@@ -0,0 +1,25 @@
+#ifndef INITIALIZE_H
+#define INITIALIZE_H
+
+#define antoxh_peiratikou 15
+#define antoxh_emporikou 10
+#define antoxh_episk 10
+#define antoxh_ekser 10
+#define STOXOS 50       //apaitoumeni posotita xrisou gia na kerdisei ena ploio
+#define LIMANIA 1   //1=80% den exei thisavro 2=70% den exei limani ktl
+#define DMG 2       //To damage pou kanoun ta peiratika ploia
+#define KERDOS 2       //Kerdos leitourgias emporikou
+#define XREWSH 2        //Xreosi leitourgias episkevastikou
+#define EPISKEVH 2               //episkeui tou episkevastikou
+#define KAKOS_KAIROS 7           //7=30% kakos kairos, 6=40% ktl
+#define DMG_KAIROU 3
+#define THISAVROS_THAL 1         //poso xrisafi dinei i thalasa an exei thisauro
+#define POSOSTO_EPISK 0.2       //To pososto episkeuis pou kanoun ta limania
+#define DMG_LIMANIOU 3
+#define THISAVROI 2 //2=70% den exei thisavro 3=60% den exei thisavro ktl
+#define XRISOS_KLOPIS 2         //O xrisos pou klevei o kleftis
+#define GOLD_CHANCE 1 //1=10%, 2=20% ktl
+#define GOLD_GENERATED 2  //to xrisafi pou mporei na kerdisei o xrisothiras me tin leitourgia tou
+
+
+#endif
